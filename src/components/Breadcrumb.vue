b775d819d81125436b6279ba394461e819c75f16
Naffah Abdulla Rasheed <mail@naffah.me>
2024-05-07 20:12:27 +0500

    refactor: Make `to` prop in Breadcrumb.vue optional

--- src/components/Breadcrumb.vue
+++ src/components/Breadcrumb.vue
@@ -2,7 +2,7 @@
 defineProps<{
   title: string,
   icon: string,
-  to: string | undefined
+  to?: string | undefined
 }>();
 </script>
 
@@ -12,7 +12,7 @@ defineProps<{
             :to="to ?? '#'"
             class="inline-flex items-center text-sm text-gray-500"
             :class="to ? 'hover:text-primary-600' : 'cursor-auto'"
-            :disabled="!to"
+            :disabled="to"
         >
             <i class="text-base me-2.5" :class="icon"></i>
             <span class="line-clamp-1">{{ title }}</span>
